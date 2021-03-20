3eab57c9c757ebbb3950857ad254bb1ead5a8d3e
Jacqueline-9 <Jacquelinerosas24@gmail.com>
2021-03-19 22:13:08 -0600

    Carga Inicial

---
+++ Punto_Venta_Abarrotes/Punto_Venta_Abarrotes/Forms/InterfazPrincipal.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Punto_Venta_Abarrotes
+{
+    public partial class InterfazPrincipal : Form
+    {
+        public InterfazPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void panelBarraTitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void AbrirFormulario(object abrirForm)
+        {
+            if (this.panelContenedor.Controls.Count > 0)
+            {
+                this.panelContenedor.Controls.RemoveAt(0);
+            }
+            Form fh = abrirForm as Form;
+            fh.TopLevel = false;
+            fh.Dock = DockStyle.Fill;
+            this.panelContenedor.Controls.Add(fh);
+            this.panelContenedor.Tag = fh;
+            fh.Show();
+        }
+
+        private void btnProductos_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario(new Productos());
+        }
+
+        private void ptbCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ptbMaximizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            ptbMaximizar.Visible = false;
+            ptbRestaurar.Visible = true;
+        }
+
+        private void ptbMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ptbRestaurar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            ptbRestaurar.Visible = false;
+            ptbMaximizar.Visible = true;
+        }
+
+        
+    }
+}
